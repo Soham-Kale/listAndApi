0ed67be058cdf996d2220c718eb497920f504b1b
Soham-Kale <sohamkale3014@gmail.com>
2026-01-10 14:57:24 +0530

    Enhance ThemeScreen with toggle functionality and dynamic styling based on theme

--- src/screen/ThemeScreen.tsx
+++ src/screen/ThemeScreen.tsx
@@ -1,9 +1,25 @@
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Switch, Text, View } from "react-native"
+import { useTheme } from "../context/ThemeContext";
 
 const ThemeScreen = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    const isDarkMode = theme === 'dark';
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.heading}>Theme Screen</Text>
+        <View style={[styles.container, {
+            backgroundColor: isDarkMode ? "#000000" : "#ffffff"
+        }]}>
+            <Text style={[styles.heading, {
+                color: isDarkMode ? "#ffffff" : "#000000"
+            }]}>Theme Screen</Text>
+
+            <Switch
+                value={isDarkMode}
+                onValueChange={toggleTheme}
+                trackColor={{false: "#767577", true: "#81b0ff"}}
+                thumbColor={isDarkMode ? "#f5dd4b" : "#f4f3f4"}
+            />
         </View>
     )
 };
