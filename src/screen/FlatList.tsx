732caf0d5c0d1390eb17bc9a230e0884de144f61
Soham-Kale <sohamkale3014@gmail.com>
2026-01-11 17:07:40 +0530

    Add theme toggle functionality to FlastListScreen with dynamic styling

--- src/screen/FlatList.tsx
+++ src/screen/FlatList.tsx
@@ -1,4 +1,5 @@
-import { FlatList, StyleSheet, Text, View } from "react-native"
+import { FlatList, StyleSheet, Switch, Text, View } from "react-native"
+import { useTheme } from "../context/ThemeContext"
 
 // data from api
 const FLAST_DATA = Array.from({length: 50}, (_, index) => ({
@@ -8,14 +9,32 @@ const FLAST_DATA = Array.from({length: 50}, (_, index) => ({
 
 const FlastListScreen: React.FC = () => {
 
+    const { theme, toggleTheme } = useTheme();
+
+    const isDarkMode = theme === 'dark';
+
     const handleRenderItem = ({item}: {item: {id: string, title: string}}) => (
         <View style={styles.item}>
-            <Text>{item.title}</Text>
+            <Text style={{
+                color: isDarkMode ? "#ffffff" : "#000000"
+            }}>{item.title}</Text>
         </View>
     )
 
-    return <View style={styles.container}>
-        <Text style={styles.header}>FlatList Example</Text>
+    return <View style={[styles.container, {
+        backgroundColor: isDarkMode ? "#000000" : "#ffffff"
+    }]}>
+        <Text style={[styles.header, {
+            color: isDarkMode ? "#ffffff" : "#000000"
+        }]}>FlatList Example</Text>
+
+        <Switch
+            value={isDarkMode}
+            onValueChange={toggleTheme}
+            thumbColor={isDarkMode?  "#f5dd4b" : "#f4f3f4"}
+            trackColor={{false: "#767577", true: "#81b0ff"}}
+        />
+
         <FlatList
             ListHeaderComponent={
                 <Text style={styles.flatListHeader}>FlatList Header</Text>
